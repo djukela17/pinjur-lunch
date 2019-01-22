8aab260cde0a34ecf3b973b858259a58e8d23419
djukela17 <gracin.julian@gmail.com>
2019-01-22 16:50:56 +0100

    [wip] add insert function for importing data from json into mongodb

--- internal/models/dish-collection.go
+++ internal/models/dish-collection.go
@@ -1,6 +1,13 @@
 package models
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/mongodb/mongo-go-driver/mongo"
+	"github.com/mongodb/mongo-go-driver/mongo/readpref"
+	"time"
+)
 
 type DishCollection struct {
 	Dishes []Dish
@@ -27,4 +34,38 @@ func (c *DishCollection) GetDish(name string) (Dish, error) {
 
 func (c *DishCollection) GetAll() []Dish {
 	return c.Dishes
+}
+
+func (c *DishCollection) InsertAll(client *mongo.Client, databaseName, collectionName string, deleteOld bool) error {
+	// ping to check if server is available
+	// this should not be necessary every time
+	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
+	err := client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return err
+	}
+
+	interfaceSlice := make([]interface{}, len(c.Dishes))
+	for i, d := range c.Dishes {
+		interfaceSlice[i] = d
+	}
+
+	collection := client.Database(databaseName).Collection(collectionName)
+	ctx, _ = context.WithTimeout(context.Background(), 5*time.Second)
+
+	if deleteOld {
+		res, err := collection.DeleteMany(ctx, bson.M{})
+		if err != nil {
+			return err
+		}
+		fmt.Println(res)
+	}
+
+	ctx, _ = context.WithTimeout(context.Background(), 5*time.Second)
+	res, err := collection.InsertMany(ctx, interfaceSlice)
+
+	fmt.Println(res.InsertedIDs)
+	fmt.Println(len(res.InsertedIDs))
+
+	return nil
 }
